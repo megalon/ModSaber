eaf38e91806270c637374a6934214a481d67a5ad
Jack Baron <jackmbaron@gmail.com>
2018-07-26 18:33:34 +0100

    Refactor constants and add redis

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const dev = process.env.NODE_ENV === 'development'
 
 module.exports = {
-  MONGO_URL: process.env.NODE_ENV === 'development' ? 'mongodb://localhost:27017/modsaber' : 'mongodb://mongo:27017/modsaber',
+  MONGO_URL: dev ? 'mongodb://localhost:27017/modsaber' : 'mongodb://mongo:27017/modsaber',
+  REDIS_HOST: dev ? 'localhost' : 'redis',
   COOKIE_NAME: 'jwt',
   STORE_PATH: path.join(__dirname, 'store'),
   errors: {
