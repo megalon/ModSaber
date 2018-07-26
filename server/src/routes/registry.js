9ca0d86de24aeeeb9f79c159fe1cf980cf063644
Jack Baron <jackmbaron@gmail.com>
2018-07-26 17:52:48 +0100

    Get mod info from registry

--- server/src/routes/registry.js
+++ server/src/routes/registry.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const semver = require('semver')
 const Mod = require('../models/mod.js')
+const Account = require('../models/account.js')
 
 // Setup Router
 const router = Router() // eslint-disable-line
@@ -48,6 +49,26 @@ router.get('/:name', async (req, res) => {
   let [latest] = mods.sort((a, b) => a.name > b.name ? 1 : b.name > a.name ? -1 : 0 || semver.rcompare(a.version, b.version))
 
   res.redirect(`/registry/${name}/${latest.version}`)
+})
+
+router.get('/:name/:version', async (req, res) => {
+  let { name: n, version: v } = req.params
+  let mod = await Mod.findOne({ name: n, version: v }).exec()
+
+  if (!mod) return res.sendStatus(404)
+  let { name, version, author: authorID, title, description, gameVersion, oldVersions, dependsOn, files } = mod
+
+  // Insert file URLs to file object
+  let { protocol, headers: { host } } = req
+  let baseURL = `${protocol}://${host}/cdn`
+  for (let x of Object.entries(files)) {
+    let [key, value] = x
+    value.url = `${baseURL}/${name}/${version}-${key}.zip`
+  }
+
+  // Lookup author username from DB
+  let author = (await Account.findById(authorID).exec()).username
+  res.send({ name, version, author, title, description, gameVersion, oldVersions, dependsOn, files })
 })
 
 module.exports = router
