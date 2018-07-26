74b3a302a6a1390dd18c86572b50b14cd9a40f55
Jack Baron <jackmbaron@gmail.com>
2018-07-26 11:30:52 +0100

    Require JWT for api route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+const passport = require('passport')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
@@ -19,7 +20,7 @@ app.use(cookieParser(COOKIE_SECRET))
 // Routes
 app.use('/', require('./routes/index.js'))
 app.use('/auth', require('./routes/auth.js'))
-app.use('/api', require('./routes/api.js'))
+app.use('/api', passport.authenticate('jwt', { session: false }), require('./routes/api.js'))
 
 // Connect to DB
 mongoose.connect(MONGO_URL)
