ea7e6f607194fdad27b49f576dd68a96b8f30f17
Jack Baron <jackmbaron@gmail.com>
2018-07-26 23:44:43 +0100

    Fetch user state on load

--- client/src/js/App.jsx
+++ client/src/js/App.jsx
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
 
+import constants from './constants.js'
+
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      loggedIn: false,
+      user: {},
+    }
+
+    this.loadUserState()
+  }
+
+  async loadUserState () {
+    try {
+      let user = await (await fetch(`${constants.BASE_URL}/api/self`, { credentials: 'include' })).json()
+      this.setState({ loggedIn: true, user })
+    } catch (err) {
+      // Silently Fail
+    }
+  }
+
   render () {
     return (
       <div>
---
+++ client/src/js/constants.js
@@ -0,0 +1,5 @@
+const constants = {
+  BASE_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '/',
+}
+
+export default constants
