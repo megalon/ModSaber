34e37f2014e1c7ab70302af9d5a36b6524488548
Jack Baron <jackmbaron@gmail.com>
2018-07-26 12:13:27 +0100

    Upload validation and processing

---
+++ src/models/mod.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const Mod = new Schema({
+  name: { type: String, lowercase: true, required: [true, "can't be blank"], index: true },
+  title: { type: String, required: [true, "can't be blank"] },
+  author: Schema.Types.ObjectId,
+  description: { type: String, default: '' },
+  approved: { type: Boolean, default: false },
+  version: String,
+  oldVersions: [String],
+  gameVersion: String,
+  files: Schema.Types.Mixed,
+  dependsOn: [String],
+})
+
+module.exports = mongoose.model('Mod', Mod)
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,17 +1,60 @@
 const { Router } = require('express')
 const fileUpload = require('express-fileupload')
-const { calculateHash, processZIP } = require('../app/api.js')
+const semver = require('semver')
+const { processZIP } = require('../app/api.js')
+const Mod = require('../models/mod.js')
 
 // Setup Router
 const router = Router() // eslint-disable-line
 router.use(fileUpload({ limits: { fileSize: 10 * 1024 * 1024 }, abortOnLimit: true }))
 
 router.post('/upload', async (req, res) => {
-  let { data } = req.files.steam
-  let hash = await calculateHash(data)
-  let files = await processZIP(data)
+  let { name, version, title, description, gameVersion, dependsOn } = req.body
+  let { steam, oculus } = req.files
 
-  res.send({ hash, files })
+  // Validate Required Fields
+  if (!name) return res.status(400).send({ field: 'name' })
+  if (!version) return res.status(400).send({ field: 'version' })
+  if (!title) return res.status(400).send({ field: 'title' })
+  if (!gameVersion) return res.status(400).send({ field: 'gameVersion' })
+
+  // Validate SemVer
+  version = semver.valid(semver.coerce(version))
+  if (version === null) return res.status(400).send({ field: 'version' })
+
+  // Default values
+  if (!description) description = ''
+  dependsOn = !dependsOn ? [] : dependsOn.split(',')
+
+  // Process Uploaded Files
+  if (!steam) return res.status(400).send({ field: 'steam' })
+  steam = await processZIP(steam.data)
+  oculus = !oculus ? undefined : await processZIP(oculus.data)
+
+  let existing = (await Mod.find({ name }).exec())
+    .sort((b, a) => semver.gt(a.version, b.version) ? 1 : semver.gt(b.version, a.version) ? -1 : 0)
+  if (existing.length > 0) {
+    let [previous] = existing
+    // Check they own the mod
+    if (!previous.author.id.equals(req.user._id.id)) return res.sendStatus(401)
+
+    // Check SemVer is newer
+    if (!semver.gt(version, previous.version)) return res.status(403).send({ error: 'semver' })
+  }
+
+  await Mod.create({
+    name,
+    title,
+    author: req.user.id,
+    description,
+    version,
+    oldVersions: [],
+    gameVersion,
+    files: { steam, oculus },
+    dependsOn,
+  })
+
+  res.send({ name, steam })
 })
 
 module.exports = router
