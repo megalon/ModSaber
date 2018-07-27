e0f251f28522eb0671197c913223583e0a1a2fe6
Jack Baron <jackmbaron@gmail.com>
2018-07-27 23:51:06 +0100

    Update Registry to include gameVersion properly Also fix 404 caching

--- server/src/routes/registry.js
+++ server/src/routes/registry.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const semver = require('semver')
 const Mod = require('../models/mod.js')
 const Account = require('../models/account.js')
+const GameVersion = require('../models/gameversion.js')
 const { REDIS_HOST } = require('../constants.js')
 
 // Setup Router
@@ -53,12 +54,12 @@ router.get('/:name', async (req, res) => {
   res.redirect(`/registry/${name}/${latest.version}`)
 })
 
-router.get('/:name/:version', cache.route(5 * 60), async (req, res) => {
+router.get('/:name/:version', cache.route({ expire: { 200: 5 * 60, xxx: 1 } }), async (req, res) => {
   let { name: n, version: v } = req.params
   let mod = await Mod.findOne({ name: n, version: v }).exec()
 
   if (!mod) return res.sendStatus(404)
-  let { name, version, author: authorID, title, description, gameVersion, oldVersions, dependsOn, conflictsWith, files } = mod
+  let { name, version, author: authorID, title, description, gameVersion: gameVersionID, oldVersions, dependsOn, conflictsWith, files } = mod
 
   // Insert file URLs to file object
   let { protocol, headers: { host } } = req
@@ -68,13 +69,16 @@ router.get('/:name/:version', cache.route(5 * 60), async (req, res) => {
     value.url = `${baseURL}/${name}/${version}-${key}.zip`
   }
 
+  // Lookup Game Version
+  let gameVersion = (await GameVersion.findById(gameVersionID).exec()).value
+
   try {
     // Lookup author username from DB
     let author = (await Account.findById(authorID).exec()).username
-    res.send({ name, version, author, authorID, title, description, gameVersion, oldVersions, dependsOn, conflictsWith, files })
+    res.send({ name, version, author, authorID, title, description, gameVersion, gameVersionID, oldVersions, dependsOn, conflictsWith, files })
   } catch (err) {
     // Send default values
-    res.send({ name, version, author: '', authorID: 0, title, description, gameVersion, oldVersions, dependsOn, conflictsWith, files })
+    res.send({ name, version, author: '', authorID: 0, title, description, gameVersion, gameVersionID, oldVersions, dependsOn, conflictsWith, files })
   }
 })
 
