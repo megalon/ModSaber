90625b49a7a30855b253e79d4cb1752212886e2e
Jack Baron <jackmbaron@gmail.com>
2018-07-27 16:41:03 +0100

    Fixed base URL for docker client

--- client/src/js/constants.js
+++ client/src/js/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-  BASE_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '/',
+  BASE_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '',
   MESSAGE_UNVERIFIED: `Your account is unverified. You cannot upload mods without first verifying your account.
 Check the email address you used to sign up for an account. (Look in spam too)`,
   auth: {
