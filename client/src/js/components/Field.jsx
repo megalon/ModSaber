1a9fdd36334fd44eb009487369a0282b5e7eff69
Jack Baron <jackmbaron@gmail.com>
2018-07-27 21:12:42 +0100

    Field with addons

--- client/src/js/components/Field.jsx
+++ client/src/js/components/Field.jsx
@@ -62,3 +62,44 @@ FieldArea.propTypes = {
   disabled: PropTypes.bool,
   onChange: PropTypes.func,
 }
+
+export const FieldAddon = props =>
+  <div className='field is-expanded' style={{ marginTop: props.isChild ? '-25px' : '' }}>
+    <label className='label'>{ props.label }</label>
+    <div className={ `field ${props.button ? 'has-addons' : ''}` }>
+      <div className='control is-fullwidth'>
+        <input
+          className='input'
+          type={ props.type }
+          placeholder={ props.placeholder }
+          disabled={ props.disabled }
+          readOnly={ props.readOnly }
+          onChange={ props.onChange }
+          onKeyPress={ e => { if (e.key === 'Enter') { props.onEnter() } } }
+          value={ props.value }
+        />
+      </div>
+      <div className='control'>
+        <a className={ `button ${props.buttonClass}` } onClick={ props.onButtonPress }>
+          { props.button }
+        </a>
+      </div>
+    </div>
+    <p className='help is-danger' style={{ marginTop: '-10px', marginBottom: '10px' }}>{ props.prompt }</p>
+  </div>
+
+FieldAddon.propTypes = {
+  value: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  button: PropTypes.string,
+  buttonClass: PropTypes.string,
+  onButtonPress: PropTypes.func,
+  isChild: PropTypes.bool,
+  prompt: PropTypes.string,
+  disabled: PropTypes.bool,
+  readOnly: PropTypes.bool,
+  onChange: PropTypes.func,
+  onEnter: PropTypes.func,
+}
