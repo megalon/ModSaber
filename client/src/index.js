4e7c54c58f350d6cb5ddc59c73004cac257eb644
Jack Baron <jackmbaron@gmail.com>
2018-07-27 14:00:11 +0100

    Add font-awesome

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import './css/overrides.css'
 
 import App from './js/App.jsx'
