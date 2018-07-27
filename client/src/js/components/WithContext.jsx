209412d3f838dd5ad6305f3244c4ffffeb6521a5
Jack Baron <jackmbaron@gmail.com>
2018-07-27 14:29:58 +0100

    HOC that adds context to props

---
+++ client/src/js/components/WithContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { UserConsumer } from '../Context.jsx'
+
+const withContext = Component => props => // eslint-disable-line
+  <UserConsumer>
+    { context => <Component { ...props } context={ context } /> }
+  </UserConsumer>
+
+export default withContext
