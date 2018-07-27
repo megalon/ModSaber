2c36a2556b5ae19bbce339fc544edaca07ebdae7
Jack Baron <jackmbaron@gmail.com>
2018-07-27 11:01:01 +0100

    Add Bulma CSS

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'bulma/css/bulma.css'
+import './css/overrides.css'
+
 import App from './js/App.jsx'
 
 ReactDOM.render(<App />, document.getElementById('root'))
