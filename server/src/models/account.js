a7e80f0c622eb29b251d5d33af547a801fd8927e
Jack Baron <jackmbaron@gmail.com>
2018-07-27 15:32:30 +0100

    Server side username validation

--- server/src/models/account.js
+++ server/src/models/account.js
@@ -3,7 +3,14 @@ const { Schema } = mongoose
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const Account = new Schema({
-  username: { type: String, lowercase: true, required: [true, "can't be blank"], index: true },
+  username: {
+    type: String,
+    lowercase: true,
+    required: [true, "can't be blank"],
+    match: [/^[a-z0-9\-_]+$/, 'is invalid'],
+    maxlength: 20,
+    index: true,
+  },
   email: { type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
   password: String,
   verified: { type: Boolean, default: false },
