af665847c1f88557f473abdb9a45f79ba192cb82
Jack Baron <jackmbaron@gmail.com>
2018-07-27 13:03:08 +0100

    Begin proper routing

--- client/src/js/Router.jsx
+++ client/src/js/Router.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import { HashRouter, Switch, Route } from 'react-router-dom'
 
-import Layout from './components/Layout.jsx'
+import asyncComponent from './components/AsyncComponent.jsx'
+
+import Home from './routes/Home.jsx'
+const AsyncLogin = asyncComponent(() => import('./routes/Login.jsx'))
 
 class Router extends Component {
   render () {
     return (
       <HashRouter>
         <Switch>
-          <Route exact path='/' component={Layout} />
+          <Route exact path='/' component={ Home } />
+          <Route path='/login' component={ AsyncLogin } />
         </Switch>
       </HashRouter>
     )
---
+++ client/src/js/routes/Login.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Layout from '../components/Layout.jsx'
+
+class Login extends Component {
+  render () {
+    return (
+      <Layout>
+        <h1 className='is-size-1 has-text-weight-semibold'>ModSaber</h1>
+      </Layout>
+    )
+  }
+}
+
+export default Login
