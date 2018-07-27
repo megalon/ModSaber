4dedbfe4b3b2948e249ce7ae7420d5c95e527d5f
Jack Baron <jackmbaron@gmail.com>
2018-07-27 17:41:45 +0100

    Add extra validation to mod schema

--- server/src/models/mod.js
+++ server/src/models/mod.js
@@ -2,10 +2,17 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const Mod = new Schema({
-  name: { type: String, lowercase: true, required: [true, "can't be blank"], index: true },
-  title: { type: String, required: [true, "can't be blank"] },
+  name: {
+    type: String,
+    lowercase: true,
+    required: [true, "can't be blank"],
+    match: [/^[a-z0-9\-_]+$/, 'is invalid'],
+    maxlength: 35,
+    index: true,
+  },
+  title: { type: String, required: [true, "can't be blank"], maxlength: 50 },
   author: Schema.Types.ObjectId,
-  description: { type: String, default: '' },
+  description: { type: String, default: '', maxlength: 1000 },
   approved: { type: Boolean, default: false },
   version: String,
   oldVersions: [String],
