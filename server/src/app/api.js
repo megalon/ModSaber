509d069032c33554fb1ca09d50a812928945031e
Jack Baron <jackmbaron@gmail.com>
2018-07-28 14:38:31 +0100

    Refactor registry mapping into API helpers

--- server/src/app/api.js
+++ server/src/app/api.js
@@ -1,5 +1,7 @@
 const crypto = require('crypto')
 const AdmZip = require('adm-zip')
+const Account = require('../models/account.js')
+const GameVersion = require('../models/gameversion.js')
 
 /**
  * Calculate the SHA-1 Hash of a File Buffer
@@ -48,4 +50,52 @@ const processZIP = async data => {
   return { hash, files }
 }
 
-module.exports = { calculateHash, processZIP }
+/**
+ * @typedef {Object} Mod
+ * @property {string} name
+ * @property {string} version
+ * @property {string} author
+ * @property {string} authorID
+ * @property {string} title
+ * @property {string} description
+ * @property {string} tag
+ * @property {string} gameVersion
+ * @property {string} gameVersionID
+ * @property {string[]} oldVersions
+ * @property {string[]} dependsOn
+ * @property {string[]} conflictsWith
+ * @property {any} files
+ */
+
+/**
+ * @param {*} mod Mod Object
+ * @param {Request} req HTTP Request
+ * @returns {Promise.<Mod>}
+ */
+const mapMod = async (mod, req) => {
+  let { name, version, author: authorID, title, description, tag, gameVersion: gameVersionID, oldVersions, dependsOn, conflictsWith, files } = mod
+
+  // Insert file URLs to file object
+  let { protocol, headers: { host } } = req
+  let baseURL = `${protocol}://${host}/cdn`
+  for (let x of Object.entries(files)) {
+    let [key, value] = x
+    value.url = `${baseURL}/${name}/${version}-${key}.zip`
+  }
+
+  // Lookup Game Version
+  let gameVersion = (await GameVersion.findById(gameVersionID).exec()).value
+
+  try {
+    // Lookup author username from DB
+    let author = (await Account.findById(authorID).exec()).username
+    return { name, version, author, authorID, title, description,
+      tag, gameVersion, gameVersionID, oldVersions, dependsOn, conflictsWith, files }
+  } catch (err) {
+    // Send default values
+    return { name, version, author: '', authorID: '0', title, description,
+      tag, gameVersion, gameVersionID, oldVersions, dependsOn, conflictsWith, files }
+  }
+}
+
+module.exports = { calculateHash, processZIP, mapMod }
--- server/src/routes/registry.js
+++ server/src/routes/registry.js
@@ -1,8 +1,7 @@
 const { Router } = require('express')
 const semver = require('semver')
 const Mod = require('../models/mod.js')
-const Account = require('../models/account.js')
-const GameVersion = require('../models/gameversion.js')
+const { mapMod } = require('../app/api.js')
 const { REDIS_HOST } = require('../constants.js')
 
 // Setup Router
@@ -23,29 +22,7 @@ router.get('/:name/:version', cache.route({ expire: { 200: 5 * 60, xxx: 1 } }), 
   let mod = await Mod.findOne({ name: n, version: v }).exec()
 
   if (!mod) return res.sendStatus(404)
-  let { name, version, author: authorID, title, description, tag, gameVersion: gameVersionID, oldVersions, dependsOn, conflictsWith, files } = mod
-
-  // Insert file URLs to file object
-  let { protocol, headers: { host } } = req
-  let baseURL = `${protocol}://${host}/cdn`
-  for (let x of Object.entries(files)) {
-    let [key, value] = x
-    value.url = `${baseURL}/${name}/${version}-${key}.zip`
-  }
-
-  // Lookup Game Version
-  let gameVersion = (await GameVersion.findById(gameVersionID).exec()).value
-
-  try {
-    // Lookup author username from DB
-    let author = (await Account.findById(authorID).exec()).username
-    res.send({ name, version, author, authorID, title, description,
-      tag, gameVersion, gameVersionID, oldVersions, dependsOn, conflictsWith, files })
-  } catch (err) {
-    // Send default values
-    res.send({ name, version, author: '', authorID: 0, title, description,
-      tag, gameVersion, gameVersionID, oldVersions, dependsOn, conflictsWith, files })
-  }
+  res.send(await mapMod(mod, req))
 })
 
 module.exports = router
