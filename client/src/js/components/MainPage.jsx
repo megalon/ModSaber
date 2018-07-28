0bce74ae7cdd95162076127825739b92a7728de6
Jack Baron <jackmbaron@gmail.com>
2018-07-29 00:03:12 +0100

    Add FAQ and Privacy buttons to mainpage component

--- client/src/js/components/MainPage.jsx
+++ client/src/js/components/MainPage.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+
 import Layout from '../components/Layout.jsx'
 
 class MainPage extends Component {
@@ -13,8 +15,17 @@ class MainPage extends Component {
     return (
       <Layout {...this.props} >
 
-        <h1 className='is-size-1 has-text-weight-semibold'>ModSaber { this.props.title ? `// ${this.props.title}` : '' }</h1>
-        <p><i>Beat Saber Mods Database</i></p>
+        <div className='columns'>
+          <div className='column is-10'>
+            <h1 className='is-size-1 has-text-weight-semibold'>ModSaber { this.props.title ? `// ${this.props.title}` : '' }</h1>
+            <p><i>Beat Saber Mods Database</i></p>
+          </div>
+
+          <div className='column home-buttons'>
+            <Link to='/faq' className='button is-dark is-inverted is-outlined is-control'>FAQ</Link>
+            <Link to='/privacy' className='button is-dark is-inverted is-outlined is-control'>Privacy</Link>
+          </div>
+        </div>
 
         { this.props.children }
       </Layout>
