6cba549b8c0f220e60b4678682e095ef12abd2a1
Jack Baron <jackmbaron@gmail.com>
2018-08-13 14:14:21 +0100

    Refactor file field to own JSX file

--- client/src/js/components/Form.jsx
+++ client/src/js/components/Form.jsx
@@ -4,6 +4,7 @@ import * as semver from 'semver'
 
 import { BASE_URL, sanitise } from '../constants.js'
 import Field, { FieldArea, FieldAddon } from './Field.jsx'
+import FileField from './FileField.jsx'
 
 class Form extends Component {
   constructor (props) {
@@ -393,32 +394,6 @@ class Form extends Component {
       </div>
     )
   }
-}
-
-const FileField = props =>
-  <div className={ `file has-name is-fullwidth ${props.isHidden ? 'is-invis' : ''}` } style={{ marginBottom: '10px' }}>
-    <label className='file-label'>
-      <input
-        className='file-input'
-        type='file'
-        accept='.zip'
-        ref={ props.node }
-        onChange={ props.onChange }
-      />
-      <span className='file-cta'>
-        <span className='file-icon'><i className='fas fa-upload'></i></span>
-        <span className='file-label'>{ props.name }</span>
-      </span>
-      <span className='file-name'>{ props.file.name }</span>
-    </label>
-  </div>
-
-FileField.propTypes = {
-  name: PropTypes.string.isRequired,
-  isHidden: PropTypes.bool,
-  node: PropTypes.any.isRequired,
-  onChange: PropTypes.func.isRequired,
-  file: PropTypes.any.isRequired,
 }
 
 export default Form
