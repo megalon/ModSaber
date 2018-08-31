b139254010ca87992e9c2d3194e9977ecdeb6469
Jack Baron <jackmbaron@gmail.com>
2018-08-31 13:54:07 +0100

    Fix author name

--- client/ssr/middleware/modmeta.js
+++ client/ssr/middleware/modmeta.js
@@ -5,15 +5,19 @@ export default async (req, res, next) => {
   const { name, version } = req.params
 
   try {
-    const collection = await mongoose.connection.db.collection('mods')
+    const mods = await mongoose.connection.db.collection('mods')
     const query = version ? { name, version, unpublished: false, approved: true } : { name, unpublished: false, approved: true }
-    const [mod] = await (await collection.find(query)).toArray()
+    const [mod] = await (await mods.find(query)).toArray()
 
     // Ignore for no mod
     if (!mod) return next()
 
+    const accounts = await mongoose.connection.db.collection('accounts')
+    const [author] = await (await accounts.find({ _id: mod.author })).toArray()
+    console.log(author)
+
     // Push meta tags to next middleware
-    req.metaTags = modMetaTags(mod.name, mod.version, mod.author, mod.title)
+    req.metaTags = modMetaTags(mod.name, mod.version, author.username || 'author', mod.title)
     return next()
   } catch (err) {
     console.error(err)
