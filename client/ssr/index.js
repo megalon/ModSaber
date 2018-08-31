17cc72f64d2a08a4ff0b08dcc858ad6112e9a54f
Jack Baron <jackmbaron@gmail.com>
2018-08-31 13:45:01 +0100

    Connect to Mongo

--- client/ssr/index.js
+++ client/ssr/index.js
@@ -3,10 +3,15 @@ import * as path from 'path'
 import Loadable from 'react-loadable'
 import express, { Router } from 'express'
 import serverRenderer from './middleware/renderer.js'
+import mongoose from 'mongoose'
 
 // Express App
 const app = express()
 const router = Router() // eslint-disable-line
+
+// Connect to Mongo
+const MONGO_URL = process.env.NODE_ENV === 'development' ? 'mongodb://localhost:27017/modsaber' : 'mongodb://mongo:27017/modsaber'
+mongoose.connect(MONGO_URL, { useNewUrlParser: true })
 
 // Root (/) should always serve our server rendered page
 router.use('^/$', serverRenderer)
