447fd6a3a2283bf1ef506cfbf47efd7543d6f57a
Jack Baron <jackmbaron@gmail.com>
2018-09-04 02:44:13 +0100

    Handle errors on legacy endpoint

--- server/src/routes/legacy.js
+++ server/src/routes/legacy.js
@@ -8,11 +8,16 @@ const router = Router() // eslint-disable-line
 const cache = require('express-redis-cache')({ host: REDIS_HOST })
 
 router.get('/api/public/temp/approved', cache.route(10), async (req, res) => {
-  let { docs, pages } = await Mod.paginate({ approved: true, unpublished: false }, { page: 1, limit: 999999, sort: '-weight name' })
-  let mods = await Promise.all(docs.map(mod => mapMod(mod, req)))
-  let lastPage = pages - 1
+  try {
+    let { docs, pages } = await Mod.paginate({ approved: true, unpublished: false }, { page: 1, limit: 999999, sort: '-weight name' })
+    let mods = await Promise.all(docs.map(mod => mapMod(mod, req)))
+    let lastPage = pages - 1
 
-  res.send({ mods, lastPage })
+    res.send({ mods, lastPage })
+  } catch (err) {
+    console.error(err)
+    res.sendStatus(500)
+  }
 })
 
 module.exports = router
