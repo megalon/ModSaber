ce24ce8bf5287dcea67c19c3bd6d3a350d74792f
Jack Baron <jackmbaron@gmail.com>
2018-09-06 01:30:12 +0100

    Optimise DOM tree

--- client/src/js/components/layout/Layout.jsx
+++ client/src/js/components/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 import withContext from '../hoc/WithContext.jsx'
@@ -9,7 +9,7 @@ import Alert from './Alert.jsx'
 const Navbar = withContext(navbar)
 
 const Layout = props =>
-  <div>
+  <Fragment>
     <Navbar history={ props.history } />
 
     <div className='container'>
@@ -19,7 +19,7 @@ const Layout = props =>
         { props.children }
       </div>
     </div>
-  </div>
+  </Fragment>
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
